a733358a257b5519037007c07c555fbd9581173d
texhnolyze <joern-g@web.de>
2014-10-29 02:09:51 +0100

    fix(fileops): fileops now work in checking whether a file exists in a directory

--- clojure/templating/src/templating/fileops.clj
+++ clojure/templating/src/templating/fileops.clj
@@ -1,25 +1,27 @@
 (ns templating.fileops
-  (:require [clojure.string :as string]))
+  (use [clojure.string :only (join)]))
 
-(defn split_path_filename
+(defn split_path
   [file_path]
-  ;(string/split file_path #"$/"))
-  (["/home/soma" "test"]))
-
-(defn path 
-  [file_path]
-  (first (split_path_filename file_path)))
+  (.split file_path "/"))
 
 (defn filename
   [file_path]
-  (last (split_path_filename file_path)))
+  (last (split_path file_path)))
+
+(defn path 
+  [file_path]
+  (join "/" 
+    (remove filename 
+      (split_path file_path))))
 
 (defn file_exists?
-  [file]
+  [file_path]
   (def directory 
-    (clojure.java.io/file (path file)))
+    (clojure.java.io/file (path file_path)))
 
-  (fn files []
-    (for [file (file-seq (directory))](.getName file)))
-
-  (contains? 'files (filename file)))
+  (contains? 
+    (set 
+      (for [file (file-seq directory)] 
+        (.getName file)))
+    (filename file_path)))
