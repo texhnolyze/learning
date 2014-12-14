2d31853fe1c22e64c831e949695e7eb1c62fd14d
texhnolyze <joern-g@web.de>
2014-12-14 22:29:30 +0100

    feat(xmlSortingAlgorithm): try different approach with a hash map

--- clojure/templating/src/templating/template_to_html.clj
+++ clojure/templating/src/templating/template_to_html.clj
@@ -7,19 +7,73 @@
 
 (defn- indentation-difference
   [first-line, second-line]
-  (- (count-whitespace first-line) (count-whitespace second-line)))
+  (- (count-whitespace first-line) 
+     (count-whitespace second-line)))
 
 (defn- trim-whitespace
   [lines-vector]
   (doseq [x [lines-vector]] (trim x)))
 
-(defn- sort-template-lines
+(defn- create-line-hash-map
   [lines]
-  (vector (first lines) 
-          (if (not (empty? (rest lines)))
-          (sort-template-lines (rest lines)))))
+  (loop [lines lines tree [] parent nil element-id 0]
+    (if (not (empty? lines))
+      (do
+        (println (str "Run number: " element-id " Args: \n" 
+                      "lines = " lines "\n"
+                      "tree = " tree "\n"
+                      "parent = " parent "\n"
+                      "element-id = " element-id "\n"))
+                      
+        (if (< (indentation-difference
+                 (first lines) 
+                 (first (rest lines))) 0)
+          (recur 
+            (vec (rest lines)) 
+            (conj tree {:id element-id, :element (first lines), :parent parent})
+            {:id element-id, :element (first lines), :parent parent}
+            (+ element-id 1))
+
+        (if (> (indentation-difference
+                 (first lines) 
+                 (first (rest lines))) 0)
+          (recur 
+            (vec (rest lines)) 
+            (conj tree {:id element-id, :element (first lines), :parent parent})
+            (parent :parent)
+            (+ element-id 1))
+
+        (if (= (indentation-difference
+                 (first lines) 
+                 (first (rest lines))) 0)
+          (recur
+            (vec (rest lines))
+            (conj tree {:id element-id :element (first lines), :parent parent})
+            parent
+            (+ element-id 1))))))
+
+      (do
+        (println tree "\n")
+        tree))))
+
+(defn- reverse-line-hash-map
+  [line-hash-map]
+  (def reverse-nodes
+    (fn [node, child-nodes]
+      (if (node :parent)
+        (reverse-nodes (node :parent) (conj child-nodes node))
+          (do 
+            (println (str "Adding " {:content child-nodes} " to " node "\n"))
+            (conj node {:content child-nodes})))))
+
+  (map 
+    (fn [node]
+      (reverse-nodes node [])) 
+    line-hash-map))
+
 
 (defn to-html
   [template-string]
-  (sort-template-lines
-    (split-lines template-string)))
+  (reverse-line-hash-map
+    (create-line-hash-map
+      (split-lines template-string))))
--- clojure/templating/test/templating/template_to_html_test.clj
+++ clojure/templating/test/templating/template_to_html_test.clj
@@ -3,13 +3,9 @@
             [templating.template-to-html :refer [to-html]]))
 
 (def template 
-  (str "html\n  div\n    a\n  div\n"))
+  (str "html\n  div\n    a\n  div"))
 
 (deftest to-html-test
   (testing "template is turned into vector"
-    (is (= (vector "html" (vector "div" (vector "a")) "div") 
-           (to-html template))))
-
-  (testing "test"
-    (is 4 2)))
-  
+    (is (= [ "html" ["div" ["a"]] "div"] 
+        (to-html template)))))
